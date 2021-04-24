2956adc7efbbb85cd6fd1affb16e86a47783039a
kowaleuro <kowaleuro@gmail.com>
2021-04-24 02:47:24 +0200

    bombOwl
    enemy2 (nie edytowałem nazwy)

--- src/model/enemy/Enemy2.java
+++ src/model/enemy/Enemy2.java
@@ -5,7 +5,7 @@ import javafx.scene.layout.Pane;
 public class Enemy2 extends Enemy{
 
     public Enemy2(double x, double y, Pane layer) {
-        super(x, y, "/graphics/enemies/enemy2.png", "/graphics/enemies/enemy2.png", "/graphics/enemies/enemy2.png", "/graphics/enemies/enemy2.png", layer);
+        super(x, y, "graphics/enemies/bombOwl.gif", "graphics/enemies/bombOwl.gif", "graphics/enemies/bombOwl.gif", "graphics/enemies/bombOwl.gif", layer);
         setFollowing(true);
         setFlying(false);
         setShooting(true);
@@ -14,7 +14,7 @@ public class Enemy2 extends Enemy{
         setBulletVelFactor(10);
         setCooldownShot(2000);
         setRemainingHealth(15);
-        setBulletPath("graphics/items/bullets/ak47Bullet.png");
+        setBulletPath("graphics/items/bullets/enemyBullets/BombOwlBullet.png");
         setDmg(1);
     }
 
